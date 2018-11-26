54af838efdd3036c845e0c2982d18468ff8484d6
SelimaCurci <seliii@hotmail.it>
2018-11-26 10:05:55 +0100

    Comment mip function

--- src/volvis/RaycastRenderer.java
+++ src/volvis/RaycastRenderer.java
@@ -111,12 +111,12 @@ public class RaycastRenderer extends Renderer implements TFChangeListener {
     }
     
     // This function linearly interpolates the value p0 and p1 given the factor (t) between [0,1] 
-    private float interpolate(float p0, float p1, float factor) {
+    private float linearInterpolate(float p0, float p1, float factor) {
     	return p0 * (1 - factor) + p1 * factor; 
     }
     
     // This function computes the intensity value for the point with coordinates coord, using trilinear interpolation
-    private short getVoxelLinearInterpolate(double[] coord) {
+    private short getVoxelLinearInterpolated(double[] coord) {
 	// Making sure that point coordinates are within the limits.
 	if (coord[0] < 0 || coord[0] > (volume.getDimX()-2) || coord[1] < 0 || coord[1] > (volume.getDimY()-2)
 	        || coord[2] < 0 || coord[2] > (volume.getDimZ()-2)) {
@@ -143,18 +143,18 @@ public class RaycastRenderer extends Renderer implements TFChangeListener {
         
         // First we interpolate along x (note: coding used for values is cyz, so c10 means y=y0+1, z=z0)
         // Bottom cube face
-	float c00=interpolate(volume.getVoxel(x0, y0, z0), volume.getVoxel(x0 + 1, y0, z0), xd);
-	float c10=interpolate(volume.getVoxel(x0, y0 + 1, z0), volume.getVoxel(x0 + 1, y0 + 1, z0), xd);
+	float c00 = linearInterpolate(volume.getVoxel(x0, y0, z0), volume.getVoxel(x0 + 1, y0, z0), xd);
+	float c10 = linearInterpolate(volume.getVoxel(x0, y0 + 1, z0), volume.getVoxel(x0 + 1, y0 + 1, z0), xd);
 	// Top cube face
-	float c01=interpolate(volume.getVoxel(x0, y0, z0 + 1), volume.getVoxel(x0 + 1, y0, z0 + 1), xd);
-	float c11=interpolate(volume.getVoxel(x0, y0 + 1,z0 + 1), volume.getVoxel(x0 + 1, y0 + 1, z0 + 1), xd); 
+	float c01 = linearInterpolate(volume.getVoxel(x0, y0, z0 + 1), volume.getVoxel(x0 + 1, y0, z0 + 1), xd);
+	float c11 = linearInterpolate(volume.getVoxel(x0, y0 + 1,z0 + 1), volume.getVoxel(x0 + 1, y0 + 1, z0 + 1), xd); 
 	    
 	// Interpolate along y (note: v0/v1 refers to whether z=z0 or z=z0+1)
-	float c0 =interpolate(c00, c10, yd);
-	float c1 =interpolate(c01, c11, yd);
+	float c0 = linearInterpolate(c00, c10, yd);
+	float c1 = linearInterpolate(c01, c11, yd);
 	    
 	// Finally we interpolate these values along z (walking through a line).
-	float c = interpolate(c0, c1, zd);
+	float c = linearInterpolate(c0, c1, zd);
 	
         // This gives us a predicted value for the point.   
 	return (short) c; 
@@ -169,13 +169,13 @@ public class RaycastRenderer extends Renderer implements TFChangeListener {
         }
     }
 
+    /* Slicer function*/
     public void slicer(double[] viewMatrix) {
 
-        // clear image
+        // Clear image
         this.clearImage();
 
-        // vector uVec and vVec define a plane through the origin, 
-        // perpendicular to the view vector viewVec
+        // Vector uVec and vVec define a plane through the origin, perpendicular to the view vector viewVec
         double[] viewVec = new double[3];
         double[] uVec = new double[3];
         double[] vVec = new double[3];
@@ -183,14 +183,14 @@ public class RaycastRenderer extends Renderer implements TFChangeListener {
         VectorMath.setVector(uVec, viewMatrix[0], viewMatrix[4], viewMatrix[8]);
         VectorMath.setVector(vVec, viewMatrix[1], viewMatrix[5], viewMatrix[9]);
 
-        // image is square
+        // Image is square
         int imageCenter = image.getWidth() / 2;
 
         double[] pixelCoord = new double[3];
         double[] volumeCenter = new double[3];
         VectorMath.setVector(volumeCenter, volume.getDimX() / 2, volume.getDimY() / 2, volume.getDimZ() / 2);
 
-        // sample on a plane through the origin of the volume data
+        // Sample on a plane through the origin of the volume data
         double max = volume.getMaximum();
         TFColor voxelColor = new TFColor();
 
@@ -203,7 +203,8 @@ public class RaycastRenderer extends Renderer implements TFChangeListener {
                 pixelCoord[2] = uVec[2] * (i - imageCenter) + vVec[2] * (j - imageCenter)
                         + volumeCenter[2];
 
-                int val = getVoxel(pixelCoord);
+                // Speed up rendering when moving volume
+                int val = this.interactiveMode ? getVoxel(pixelCoord) : getVoxelLinearInterpolated(pixelCoord);
                 
                 // Map the intensity to a grey value by linear scaling
                 voxelColor.r = val/max;
@@ -219,18 +220,19 @@ public class RaycastRenderer extends Renderer implements TFChangeListener {
                 int c_green = voxelColor.g <= 1.0 ? (int) Math.floor(voxelColor.g * 255) : 255;
                 int c_blue = voxelColor.b <= 1.0 ? (int) Math.floor(voxelColor.b * 255) : 255;
                 int pixelColor = (c_alpha << 24) | (c_red << 16) | (c_green << 8) | c_blue;
+                
                 image.setRGB(i, j, pixelColor);
             }
         }
     }
 
+    /* Maximum Intensity Projection function */
     public void mip(double[] viewMatrix) {
 
-        // clear image
+        // Clear image
         this.clearImage();
 
-        // vector uVec and vVec define a plane through the origin, 
-        // perpendicular to the view vector viewVec
+        // Vector uVec and vVec define a plane through the origin, perpendicular to the view vector viewVec
         double[] viewVec = new double[3];
         double[] uVec = new double[3];
         double[] vVec = new double[3];
@@ -238,38 +240,45 @@ public class RaycastRenderer extends Renderer implements TFChangeListener {
         VectorMath.setVector(uVec, viewMatrix[0], viewMatrix[4], viewMatrix[8]);
         VectorMath.setVector(vVec, viewMatrix[1], viewMatrix[5], viewMatrix[9]);
 
-        // image is square
+        // Image is square
         int imageCenter = image.getWidth() / 2;
 
         double[] pixelCoord = new double[3];
         double[] volumeCenter = new double[3];
         VectorMath.setVector(volumeCenter, volume.getDimX() / 2, volume.getDimY() / 2, volume.getDimZ() / 2);
 
-        // sample on a plane through the origin of the volume data
+        // Sample on a plane through the origin of the volume data
         double max = volume.getMaximum();
         TFColor voxelColor = new TFColor();
 
+        // Step size of samples akong the viewing ray
         step = this.interactiveMode ? INTERACTIVE_MODE_STEP : NON_INTERACTIVE_MODE_STEP;
         
-        int[] kRange = new int[2];
+        // Array to store the entry and exit poin of the ray
+        int[] range = new int[2];
+        int entryPoint;
+        int exitPoint;
         
-         // Ray computation for each pixel
+        // Ray computation for each pixel
         for (int j = 0; j < image.getHeight(); j++) {
             for (int i = 0; i < image.getWidth(); i++) {
 
                 // Initialize max intensity
                 int maxIntensity = 0;
-                // Compute the entry and exit point of the ray
-                kRange = optimalDepth(imageCenter, viewVec, uVec, vVec, i, j);
                 
-                for (int k = kRange[0]; k < kRange[1]; k+=step) {
-                    // Get calculate new volumeCenter
+                // Compute the optimal entry and exit point of the ray for the current pixel
+                range = optimalRayRange(i, j, viewVec, uVec, vVec, imageCenter);
+                entryPoint = range[0];
+                exitPoint = range[1];
+                
+                for (int k = entryPoint; k < exitPoint; k+=step) {
+             
                     pixelCoord[0] = uVec[0] * (i - imageCenter) + vVec[0] * (j - imageCenter) + viewVec[0] * (k) + volumeCenter[0];
                     pixelCoord[1] = uVec[1] * (i - imageCenter) + vVec[1] * (j - imageCenter) + viewVec[1] * (k) + volumeCenter[1];
                     pixelCoord[2] = uVec[2] * (i - imageCenter) + vVec[2] * (j - imageCenter) + viewVec[2] * (k) + volumeCenter[2];
 
                     // Speed up rendering when moving volume
-                    int val = this.interactiveMode ? getVoxel(pixelCoord) : getVoxelLinearInterpolate(pixelCoord);
+                    int val = this.interactiveMode ? getVoxel(pixelCoord) : getVoxelLinearInterpolated(pixelCoord);
                     
                     // Store maximum value
                     if (val > maxIntensity) {
@@ -295,63 +304,68 @@ public class RaycastRenderer extends Renderer implements TFChangeListener {
         }
     }
     
-    private int[] optimalDepth(int imageCenter, double[] viewVec, double[] uVec, double[] vVec, int i, int j) {
-        int kStart = Integer.MIN_VALUE;
-        int kEnd = Integer.MAX_VALUE;
+    /* Compute the optimal entry and exit point of the ray */
+    private int[] optimalRayRange(int i, int j, double[] viewVec, double[] uVec, double[] vVec, int imageCenter) {
+        // Initialize entry and exit point of teh ray
+        int start = Integer.MIN_VALUE;
+        int end = Integer.MAX_VALUE;
         
-        // Set volume dimensions volume[x,y,z][low,high] = vloume[0,1,2][0,1]
+        // Set volume dimensions: volume[x, y, z][low, high] = volume[0, 1, 2][0, 1]
         int[][] volumeBoundary = new int[3][2];
-        volumeBoundary[0][0] = -volume.getDimX() / 2;
+        volumeBoundary[0][0] = - volume.getDimX() / 2;
         volumeBoundary[0][1] = volume.getDimX() / 2;
-        volumeBoundary[1][0] = -volume.getDimY() / 2;
+        volumeBoundary[1][0] = - volume.getDimY() / 2;
         volumeBoundary[1][1] = volume.getDimY() / 2;
-        volumeBoundary[2][0] = -volume.getDimZ() / 2;
+        volumeBoundary[2][0] = - volume.getDimZ() / 2;
         volumeBoundary[2][1] = volume.getDimZ() / 2;
 
-        //origin[x,y,z]
+        // Origin [x, y, z]
         double[] origin = new double[3];
+        // Range [entryPoint, exitPoint]
+        int[] range = new int[2];
 
-        int[] kList = new int[2];
+        for (int c = 0; c < 3; c++) {
 
-        for (int l = 0; l < 3; l++) {
+            // Set origin: origin[x, y, z] = origin[0, 1, 2]
+            origin[c] = (i - imageCenter) * uVec[c] + (j - imageCenter) * vVec[c];
 
-            //set origin  origin[x,y,z] = origin[0,1,2]
-            origin[l] = (i - imageCenter) * uVec[l] + (j - imageCenter) * vVec[l];
-
-            //if origin not between the slabs then return
-            if (viewVec[l] == 0) {
-                if ((origin[l] < volumeBoundary[l][0] || origin[l] > volumeBoundary[l][1])) {
+            // If the origin is not between the boundaries then return range = [0, 0]
+            if (viewVec[c] == 0) {
+                if ((origin[c] < volumeBoundary[c][0] || origin[c] > volumeBoundary[c][1])) {
                     return new int[]{0, 0};
                 }
             } else {
 
-                //for each dimension find kLow/kHigh . k[x,y,z][low,high] : k[0,1,2][0,1]
-                kList[0] = (int) ((volumeBoundary[l][0] - origin[l]) / viewVec[l]);
-                kList[1] = (int) ((volumeBoundary[l][1] - origin[l]) / viewVec[l]);
+                // Otherwise for each dimension find low/high . range[x,y,z][low, high] : range[0, 1, 2][0, 1]
+                range[0] = (int) ((volumeBoundary[c][0] - origin[c]) / viewVec[c]);
+                range[1] = (int) ((volumeBoundary[c][1] - origin[c]) / viewVec[c]);
 
-                // if kLow > kHigh, swap
-                if (kList[0] > kList[1]) {
-                    int tmp = kList[0];
-                    kList[0] = kList[1];
-                    kList[1] = tmp;
+                // If low > high, swap
+                if (range[0] > range[1]) {
+                    int tmp = range[0];
+                    range[0] = range[1];
+                    range[1] = tmp;
                 }
-                //if kLow > kStart , kStart = kLow
-                if (kList[0] > kStart) {
-                    kStart = kList[0];
+                // If low > start , start = low
+                if (range[0] > start) {
+                    start = range[0];
                 }
-                //if kHigh < kEnd , kEnd = kHigh
-                if (kList[1] < kEnd) {
-                    kEnd = kList[1];
+                // If high < end , end = high
+                if (range[1] < end) {
+                    end = range[1];
                 }
-                //
-                if (kStart > kEnd || kEnd < 0) {
+                
+                if (start > end || end < 0) {
                     return new int[]{0, 0};
                 }
             }
         }
-        return new int[]{kStart, kEnd};
+        
+        // Return entry and exit point of the ray
+        return new int[]{start, end};
     }
     
+    /* Compositing ray function */
     void compositing(double[] viewMatrix) {
         
         // Clear the image
@@ -366,35 +380,44 @@ public class RaycastRenderer extends Renderer implements TFChangeListener {
         VectorMath.setVector(uVec, viewMatrix[0], viewMatrix[4], viewMatrix[8]);
         VectorMath.setVector(vVec, viewMatrix[1], viewMatrix[5], viewMatrix[9]);
         
-        // image is square
+        // Image is square
         int imageCenter = image.getWidth() / 2;
 
         double[] pixelCoord = new double[3];
         double[] volumeCenter = new double[3];
         VectorMath.setVector(volumeCenter, volume.getDimX() / 2, volume.getDimY() / 2, volume.getDimZ() / 2);
 
-        // sample on a plane through the origin of the volume data
+        // Sample on a plane through the origin of the volume data
         TFColor voxelColor = new TFColor();
 
         short[][] sumIntensity = new short[image.getHeight()][image.getWidth()];
-        int[] kRange = new int[2];
+        
+        // Array to store the entry and exit point of the ray
+        int[] range = new int[2];
+        int entryPoint;
+        int exitPoint;
 
+         // Step size of samples akong the viewing ray
         step = this.interactiveMode ? INTERACTIVE_MODE_STEP : NON_INTERACTIVE_MODE_STEP;
 
         for (int j = 0; j < image.getHeight(); j ++) {
             for (int i = 0; i < image.getWidth(); i ++) {
                 sumIntensity[i][j] = 0;
-                kRange = optimalDepth(imageCenter, viewVec, uVec, vVec, i, j);
-
+                
+                // Compute the optimal entry and exit point of the ray for the current pixel
+                range = optimalRayRange(i, j, viewVec, uVec, vVec, imageCenter);
+                entryPoint = range[0];
+                exitPoint = range[1];
+        
                 TFColor compositingColor = new TFColor(0, 0, 0, 0);
-                for (int k = kRange[0]; k < kRange[1]; k+=step) {
+                for (int k = entryPoint; k < exitPoint; k+=step) {
                     // Get calculate new volumeCenter
                     pixelCoord[0] = uVec[0] * (i - imageCenter) + vVec[0] * (j - imageCenter) + viewVec[0] * (k) + volumeCenter[0];
                     pixelCoord[1] = uVec[1] * (i - imageCenter) + vVec[1] * (j - imageCenter) + viewVec[1] * (k) + volumeCenter[1];
                     pixelCoord[2] = uVec[2] * (i - imageCenter) + vVec[2] * (j - imageCenter) + viewVec[2] * (k) + volumeCenter[2];
 
                     // Speed up rendering when moving volume
-                    int val = this.interactiveMode ? getVoxel(pixelCoord) : getVoxelLinearInterpolate(pixelCoord);
+                    int val = this.interactiveMode ? getVoxel(pixelCoord) : getVoxelLinearInterpolated(pixelCoord);
 
                     voxelColor = tFunc.getColor(val);
 
